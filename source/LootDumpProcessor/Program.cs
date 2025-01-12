5655eeb29998af5dede7bfcbb909685ed7550209
bluextx <bluextchannel@gmail.com>
2025-01-13 02:15:24 +0300

    Refactored service registration into dedicated extension methods

--- source/LootDumpProcessor/Program.cs
+++ source/LootDumpProcessor/Program.cs
@@ -1,15 +1,5 @@
 using LootDumpProcessor.Process;
-using LootDumpProcessor.Process.Processor.DumpProcessor;
-using LootDumpProcessor.Process.Processor.FileProcessor;
-using LootDumpProcessor.Process.Processor.v2.AmmoProcessor;
-using LootDumpProcessor.Process.Processor.v2.LooseLootProcessor;
-using LootDumpProcessor.Process.Processor.v2.StaticContainersProcessor;
-using LootDumpProcessor.Process.Processor.v2.StaticLootProcessor;
-using LootDumpProcessor.Process.Reader.Filters;
-using LootDumpProcessor.Process.Reader.Intake;
-using LootDumpProcessor.Storage;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 
 namespace LootDumpProcessor;
 
@@ -18,33 +8,11 @@ public static class Program
     public static async Task Main()
     {
         var services = new ServiceCollection();
-        RegisterServices(services);
+        services.AddLootDumpProcessor();
 
         await using var serviceProvider = services.BuildServiceProvider();
 
-        // startup the pipeline
         var pipeline = serviceProvider.GetRequiredService<IPipeline>();
         await pipeline.Execute();
-    }
-
-    private static void RegisterServices(ServiceCollection services)
-    {
-        services.AddLogging(configure => configure.AddConsole());
-
-        services.AddTransient<IStaticLootProcessor, StaticLootProcessor>();
-        services.AddTransient<IStaticContainersProcessor, StaticContainersProcessor>();
-        services.AddTransient<IAmmoProcessor, AmmoProcessor>();
-        services.AddTransient<ILooseLootProcessor, LooseLootProcessor>();
-
-        services.AddSingleton<ITarkovItemsProvider, TarkovItemsProvider>();
-        services.AddSingleton<IDataStorage>(_ => DataStorageFactory.GetInstance());
-        services.AddSingleton<IKeyGenerator, NumericKeyGenerator>();
-        services.AddTransient<IComposedKeyGenerator, ComposedKeyGenerator>();
-
-        services.AddTransient<IIntakeReader, JsonFileIntakeReader>();
-        services.AddTransient<IFileFilter, JsonDumpFileFilter>();
-        services.AddTransient<IFileProcessor, FileProcessor>();
-        services.AddTransient<IDumpProcessor, MultithreadSteppedDumpProcessor>();
-        services.AddTransient<IPipeline, QueuePipeline>();
     }
 }
---
+++ source/LootDumpProcessor/ServiceCollectionExtensions.cs
@@ -0,0 +1,42 @@
+﻿using LootDumpProcessor.Process;
+using LootDumpProcessor.Process.Processor.DumpProcessor;
+using LootDumpProcessor.Process.Processor.FileProcessor;
+using LootDumpProcessor.Process.Processor.v2.AmmoProcessor;
+using LootDumpProcessor.Process.Processor.v2.LooseLootProcessor;
+using LootDumpProcessor.Process.Processor.v2.StaticContainersProcessor;
+using LootDumpProcessor.Process.Processor.v2.StaticLootProcessor;
+using LootDumpProcessor.Process.Reader.Filters;
+using LootDumpProcessor.Process.Reader.Intake;
+using LootDumpProcessor.Storage;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace LootDumpProcessor;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddLootDumpProcessor(this ServiceCollection services)
+    {
+        services.AddLogging(configure => configure.AddConsole());
+        services.RegisterProcessors();
+
+        services.AddSingleton<ITarkovItemsProvider, TarkovItemsProvider>();
+        services.AddSingleton<IDataStorage>(_ => DataStorageFactory.GetInstance());
+        services.AddSingleton<IKeyGenerator, NumericKeyGenerator>();
+        services.AddTransient<IComposedKeyGenerator, ComposedKeyGenerator>();
+
+        services.AddTransient<IIntakeReader, JsonFileIntakeReader>();
+        services.AddTransient<IFileFilter, JsonDumpFileFilter>();
+        services.AddTransient<IFileProcessor, FileProcessor>();
+        services.AddTransient<IDumpProcessor, MultithreadSteppedDumpProcessor>();
+        services.AddTransient<IPipeline, QueuePipeline>();
+    }
+
+    private static void RegisterProcessors(this IServiceCollection services)
+    {
+        services.AddTransient<IStaticLootProcessor, StaticLootProcessor>();
+        services.AddTransient<IStaticContainersProcessor, StaticContainersProcessor>();
+        services.AddTransient<IAmmoProcessor, AmmoProcessor>();
+        services.AddTransient<ILooseLootProcessor, LooseLootProcessor>();
+    }
+}
