048a64f66358a44d1919f7c648c0c1c75adc4439
bluextx <bluextchannel@gmail.com>
2025-01-13 02:23:15 +0300

    Added configuration binding and environment variables support

--- source/LootDumpProcessor/ServiceCollectionExtensions.cs
+++ source/LootDumpProcessor/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using LootDumpProcessor.Process;
+﻿using LootDumpProcessor.Model.Config;
+using LootDumpProcessor.Process;
 using LootDumpProcessor.Process.Processor.DumpProcessor;
 using LootDumpProcessor.Process.Processor.FileProcessor;
 using LootDumpProcessor.Process.Processor.v2.AmmoProcessor;
@@ -8,6 +9,7 @@ using LootDumpProcessor.Process.Processor.v2.StaticLootProcessor;
 using LootDumpProcessor.Process.Reader.Filters;
 using LootDumpProcessor.Process.Reader.Intake;
 using LootDumpProcessor.Storage;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -18,7 +20,8 @@ public static class ServiceCollectionExtensions
     public static void AddLootDumpProcessor(this ServiceCollection services)
     {
         services.AddLogging(configure => configure.AddConsole());
-        services.RegisterProcessors();
+        AddConfiguration(services);
+        RegisterProcessors(services);
 
         services.AddSingleton<ITarkovItemsProvider, TarkovItemsProvider>();
         services.AddSingleton<IDataStorage>(_ => DataStorageFactory.GetInstance());
@@ -32,11 +35,23 @@ public static class ServiceCollectionExtensions
         services.AddTransient<IPipeline, QueuePipeline>();
     }
 
-    private static void RegisterProcessors(this IServiceCollection services)
+    private static void RegisterProcessors(IServiceCollection services)
     {
         services.AddTransient<IStaticLootProcessor, StaticLootProcessor>();
         services.AddTransient<IStaticContainersProcessor, StaticContainersProcessor>();
         services.AddTransient<IAmmoProcessor, AmmoProcessor>();
         services.AddTransient<ILooseLootProcessor, LooseLootProcessor>();
+    }
+
+    private static void AddConfiguration(IServiceCollection services)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("Config/config.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        services.AddOptions<Config>()
+            .Bind(configuration)
+            .ValidateOnStart();
     }
 }
